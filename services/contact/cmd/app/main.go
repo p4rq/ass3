b7831ddd775f1f0152c133103ef8706be18c037e
p4rq <anuarbek0471@gmail.com>
2024-02-17 21:57:11 +0600

    qq

--- services/contact/cmd/app/main.go
+++ services/contact/cmd/app/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"architecture_go/pkg/store"
+)
 
 func main() {
+	conn, err := postgres.New(postgres.Settings{})
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Pool.Close()
+
+	fmt.Println(conn.Pool.Stat())
+
 	fmt.Println("Hello World!")
 }
